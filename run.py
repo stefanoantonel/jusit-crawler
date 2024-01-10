df2864018d2f243cfb9caa6262580ab6173d51a2
stefanoantonel <stefanoantonel@users.noreply.github.com>
2024-01-10 21:27:12 +0100

    Add check when link is not full url

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-# list_link = 'https://www.jusit.ch/fr/smartphones.html?brand=Apple&model=iPhone+15+5Gjusit&priceMin=100&priceMax=1100'
-list_link = 'https://www.jusit.ch/fr/smartphones.html?brand=Apple&model=iPhone+14jusit&priceMin=100&priceMax=1100'
+list_link = 'https://www.jusit.ch/fr/smartphones.html?brand=Apple&model=iPhone+15+5Gjusit'
+# list_link = 'https://www.jusit.ch/fr/smartphones.html?brand=Apple&model=iPhone+14jusit'
 
 options = webdriver.ChromeOptions()
+options.add_argument("--no-sandbox");
+options.add_argument("--disable-dev-shm-usage");
 options.add_argument("headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
 browser = webdriver.Chrome(options=options)
-browser.implicitly_wait(30)
+browser.implicitly_wait(1)
 
 browser.get(list_link)
 
@@ -22,6 +22,8 @@ if len(items) == 2:
 link_from_list = items[0]
 detail_link = link_from_list.get_attribute('href')
 print(detail_link)
+if detail_link in ['https://www.jusit.ch/#', '#']:
+    raise Exception('Detail link not fetch properly')
 browser.get(detail_link)
 not_found_titles = browser.find_elements(By.CSS_SELECTOR, 'h4')
 for not_found in not_found_titles:
